baed82bb2c1104307b5f43f9df1d88b98b63a335
rhokeys <r3nat0hs5@gmail.com>
2021-03-09 19:11:08 -0800

    remove trash

--- index.php
+++ index.php
@@ -866,7 +866,6 @@ var elementorFrontendConfig = {"environmentMode":{"edit":false,"wpPreview":false
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",(function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())}),!1);
 	</script>
 		<div style="display:none;">
-<iframe src="./assets_files/BYW1172540.html"></iframe>
 </div>
 <style>
 .box-wpp{
